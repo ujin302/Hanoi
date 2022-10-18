6cb8c541a2945cadcb4fae2c8a45c8b1732a6652
유진 <ujin302@gmail.com>
2022-10-18 17:04:27 +0900

    코드 수정

--- Hanoi.js
+++ Hanoi.js
@@ -1,26 +1,26 @@
-var NUM_OF_DISKS = 6 // 원반 수
-var NUM_OF_COLUMNS = 3 // 움직일 수 있는 구역 수
+let NUM_OF_DISKS = 6 // 원반 수
+let NUM_OF_COLUMNS = 3 // 움직일 수 있는 구역 수
 
-var diskData; // 디스크 정보 
-var grabData; // 잡은 디스크 정보 
+let diskData; // 디스크 정보 
+let grabData; // 잡은 디스크 정보 
 
-var mouseLocation; // 마우스 위치
+let mouseLocation; // 마우스 위치
 
 
 // 컬럼 초기화
-var column = new Array(NUM_OF_COLUMNS); // 컬럼 배열 선언
-for (var i = 0; i < column.length; i++) { // 3
-  var idText = "column"+i; // 컬럼 id 값 넣을 변수 선언
+let column = new Array(NUM_OF_COLUMNS); // 컬럼 배열 선언
+for (let i = 0; i < column.length; i++) { // 3
+  let idText = "column"+i; // 컬럼 id 값 넣을 변수 선언
   column[i] = document.getElementById(idText); // 컬럼 id 값 초기화
 }
 // 디스크 초기화
-var disk = new Array(NUM_OF_DISKS); // 디스크 배열 선언
-for (var i = 0; i < disk.length; i++) { // 5
+let disk = new Array(NUM_OF_DISKS); // 디스크 배열 선언
+for (let i = 0; i < disk.length; i++) { // 5
   disk[i] = new Array(NUM_OF_COLUMNS); // 디스크의 컬럼 구역 배열 선언
 }
 
-for (var i = 0; i < disk.length; i++) { // 6
-  for (var j = 0; j < disk[j].length; j++) {
+for (let i = 0; i < disk.length; i++) { // 6
+  for (let j = 0; j < disk[j].length; j++) {
     var idText = 'disk'+i+j;
     disk[i][j] = document.getElementById(idText);
     console.log(disk[i][j]);
@@ -31,7 +31,7 @@ for (var i = 0; i < disk.length; i++) { // 6
 var diskWidth = [40,80,110,140,170,200];
 var diskColor = ["black","violet","teal","tomato","turquoise","yellowgreen"];
 
-function Init() {
+const Init = () => {
   mouseLocation = -1;
   diskData = [
     [1,0,0],
@@ -46,11 +46,10 @@ function Init() {
   draw();
 }
 
-
-
-function getDiskInfo(diskNum) {
-var  retrunWidth;
-var retrunColor;
+const getDiskInfo = (diskNum) => {
+  
+let  retrunWidth;
+let retrunColor;
 
   switch(diskNum){
      case 1 :
@@ -79,10 +78,9 @@ var retrunColor;
        break;
    }
    return { retrunWidth, retrunColor };
-  }
+}
 
-
-function draw() {
+const draw = () => {
   for (var i = 0; i < diskData.length; i++) { // 6
     for (var j = 0; j < diskData[0].length; j++) { //3
       if(diskData[i][j] == 0){
@@ -97,29 +95,23 @@ function draw() {
 
 Init();
 
-function grabDisk(columnNum){
+const grabDisk = (columnNum) => {
   for (var i = 0; i < NUM_OF_DISKS; i++) {
     if(diskData[i][columnNum] > 0){
       grabData.isGrab = true;
       grabData.location = columnNum;
       grabData.i = i;
       grabData.diskValue = diskData[i][columnNum];
-      // console.log(diskData[i][columnNum]);
-      // console.log(grabData.diskValue); 
       diskData[i][columnNum] = 0;
-      // console.log(diskData[i][columnNum]);
       break;
     }
   }
 }
 
-
-function dropDisk(columnNum){
+const dropDisk = (columnNum) => {
   if(diskData[NUM_OF_DISKS-1][columnNum] == 0){ // [6-1]
     grabData.isGrab = false;
     diskData[NUM_OF_DISKS-1][columnNum] = grabData.diskValue;
-    // console.log(grabData.diskValue);
-    // console.log(diskData[NUM_OF_DISKS-1][columnNum]);
     return;
   }
 
@@ -141,23 +133,22 @@ function dropDisk(columnNum){
   draw();
 }
 
-function mouseOver(columnNum){
+const mouseOver = (columnNum) => {
   mouseLocation = columnNum;
   draw();
 }
 
-function mouseOut(columnNum){
+const mouseOut = () => {
   mouseLocation = -1
   draw()
 }
 
-function mouseClick(columnNum){
+const mouseClick = (columnNum) => {
   if(grabData.isGrab == false){
     grabDisk(columnNum);
     
   }else{
     dropDisk(columnNum)
-    // column[columnNum].style.backgroundColor = '#accaaf3'
   }
   draw()
 }
